ea2b22a4392dcecb29378be1177baccc7bcda459
Oubey-Chaabeni <chaabeniouney@gmail.com>
2025-10-13 21:27:57 +0100

    1.2.0

--- src/clientpackage/Client.java
+++ src/clientpackage/Client.java
@@ -2,23 +2,37 @@ package clientpackage;
 
 import java.io.IOException;
 import java.net.Socket;
-
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) 
     {
+        Scanner intScanner = new Scanner(System.in);
+
+        int x;
         try
         {
 
-            // La première étape :  
-
             System.out.println("Je suis un client pas encore connecté…");  
+            
             Socket socket = new Socket("localhost", 1234);
-            
-            
             System.out.println("je suis un client connecté");  
-            // La dernière étape : Fermer socket 
+
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            
+            System.out.println("Entrer l entier pour l envoyer au serveur : ");
+            x = intScanner.nextInt();
+            out.writeInt(x);
+
+            x = in.readInt();
+
+            System.out.println("La resulta envoi par le serveur est :"+x+".");
+
             socket.close();
+
         }catch(IOException e )
         {
             e.printStackTrace();
--- src/serverpackage/Server.java
+++ src/serverpackage/Server.java
@@ -3,23 +3,34 @@ package serverpackage;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 
 public class Server {
     public static void main(String[] args) 
     {
-        //La premiere etape : 
+
+        int x;
 
         try(ServerSocket serverSocket = new ServerSocket(1234))
         {
         System.out.println("Je suis un serveur en attente la connexion d'un client");
 
-        //La deuxieme etape : 
-
         Socket clientSocket = serverSocket.accept();
         System.out.println("un client est connecté"); 
 
-        // La dernière étape : Fermer socket 
+        DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+        DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+
+        x = in.readInt();
+
+        x*=5;
+
+        out.writeInt(x);
+        System.out.println("Resulta envoie succefully!");
+
         clientSocket.close();
+
         }catch(IOException e)
         {
             e.printStackTrace();
